782df0e1c38825a07795720dfe0a21ace170e613
Arjun C <arjunchandrakumar2017@gmail.com>
2026-03-29 19:14:39 +0530

    auto migrate on vercel

--- bill_tracker/asgi.py
+++ bill_tracker/asgi.py
@@ -3,4 +3,16 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bill_tracker.settings')
 
+# 👇 ADD THIS BLOCK
+import django
+django.setup()
+
+from django.core.management import call_command
+
+try:
+    call_command('migrate')
+except Exception:
+    pass
+
+# 👇 existing
 application = get_asgi_application()
