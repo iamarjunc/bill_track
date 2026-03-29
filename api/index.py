89480e770bccb523d07935765a6d19aa9df27d67
Arjun C <arjunchandrakumar2017@gmail.com>
2026-03-29 19:49:07 +0530

    run migrations on vercel

--- api/index.py
+++ api/index.py
@@ -1,18 +1,18 @@
 import os
 import sys
 
-# 👇 FIX PATH (IMPORTANT)
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, BASE_DIR)
 
-# 👇 Correct settings path
 os.environ["DJANGO_SETTINGS_MODULE"] = "bill_tracker.settings"
 
-# 👇 Initialize Django
 import django
 django.setup()
 
-# 👇 Load ASGI
+# 🔥 ADD THIS BLOCK
+from django.core.management import call_command
+call_command("migrate", interactive=False)
+
 from django.core.asgi import get_asgi_application
 
 application = get_asgi_application()
